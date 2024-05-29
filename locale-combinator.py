1d5f494891d136f19335a87f5f1f8be4eaf98e2a
XCyberCode <mat.vasenin@yandex.ru>
2024-05-29 13:35:51 +0300

    [LocaleCombinator] Добавлен функционал исправения регистра

--- locale-combinator.py
+++ locale-combinator.py
@@ -1,6 +1,9 @@
 import json, argparse
+from dataclasses import dataclass
 
-changes_amount = 0
+@dataclass
+class op_counter:
+    changes: int = 0
 
 arg_parser = argparse.ArgumentParser(
     prog = "Locale Combinator", 
@@ -9,6 +12,7 @@ arg_parser = argparse.ArgumentParser(
 arg_parser.add_argument("eng_source", type = str, help = "Оригинальный файл (английский язык)")
 arg_parser.add_argument("locale_source", type = str, help = "Устаревший файл локализации")
 arg_parser.add_argument("destination", type = str, help = "Объединенный файл (вывод)")
+arg_parser.add_argument("--case", help = "Включить исправление регистра строк", action = "store_true")
 args = arg_parser.parse_args()
 
 with open(args.eng_source, encoding = "utf-8") as f:
@@ -20,10 +24,15 @@ with open(args.locale_source, encoding = "utf-8") as f:
 for key, value in english_json.items():
     if(key in old_language_json):
         english_json[key] = old_language_json.get(key)
-        changes_amount += 1
+        op_counter.changes += 1
+    
+    if(args.case):
+        english_json[key] = english_json[key].capitalize()
 
 with open(args.destination, "w", encoding = "utf-8") as f:
     json.dump(english_json, f, ensure_ascii = False, indent = 2, sort_keys = True)
 
-print(f"Всего: {len(english_json)}")
-print(f"Изменено: {changes_amount}")
+print(f"Всего строк: {len(english_json)}")
+print(f"Существует перевод: {op_counter.changes}")
+if(args.case):
+    print("Выполнено исправление регистра.")
